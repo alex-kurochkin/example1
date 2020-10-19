033d7dd90c06ee5818e5ffecbbde30f8629ee4f7
a.kurochkin <inspector@list.ru>
2020-10-19 18:10:32 +0600

    AddrObjFiasMapper. Fix Level field name.

--- models/parsers/FIAS/mappers/AddrObjFiasMapper.php
+++ models/parsers/FIAS/mappers/AddrObjFiasMapper.php
@@ -34,7 +34,7 @@ class AddrObjFiasMapper extends AbstractFiasMapper
         'NAME' => 'name',
         'TYPENAME' => 'type_name',
         'LEVEL' => [
-            'name' => 'string',
+            'name' => 'level',
             'value' => [
                 'type' => 'int',
                 'nullable' => false,
