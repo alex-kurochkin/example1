2454ffd4f4e30fbb6a2a8094030e14b7f4c36676
a.kurochkin <inspector@list.ru>
2020-10-23 18:54:28 +0600

    Drop unused code

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -7,7 +7,6 @@ namespace app\controllers;
 use app\controllers\actions\SearchAction;
 use app\controllers\actions\StatusAction;
 use yii\base\Controller;
-use yii\web\ErrorAction;
 
 class ApiController extends Controller
 {
@@ -15,15 +14,8 @@ class ApiController extends Controller
     public function actions(): array
     {
         return [
-            'error' => ErrorAction::class,
             'status' => StatusAction::class,
             'search' => SearchAction::class,
-
-            // declares "view" action using a configuration array
-//            'view' => [
-//                'class' => 'yii\web\ViewAction',
-//                'viewPrefix' => '',
-//            ],
         ];
     }
 }
