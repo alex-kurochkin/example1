31c85c7a2c088e2bce1c235097449dac82ba5909
a.kurochkin <inspector@list.ru>
2020-10-23 16:51:11 +0600

    AbstractFiasModel add mapping DB to Model

--- models/FIAS/AbstractFiasModel.php
+++ models/FIAS/AbstractFiasModel.php
@@ -9,12 +9,23 @@ use Exception;
 use RuntimeException;
 use Yii;
 use yii\base\Model;
-use yii\db\ActiveRecord;
+use yii\db\Query;
 
-abstract class AbstractFiasModel extends /*Model*/ ActiveRecord
+abstract class AbstractFiasModel extends Model
 {
 
     protected array $map;
+    protected array $mapToModel;
+
+    protected Query $query;
+
+    public function __construct(Query $query, $config = [])
+    {
+        $this->query = $query;
+        $this->mapToModel = array_flip($this->map);
+
+        parent::__construct($config);
+    }
 
     public static function getModel(string $name): self
     {
@@ -51,5 +62,56 @@ abstract class AbstractFiasModel extends /*Model*/ ActiveRecord
         } catch (Exception $e) {
             throw new FiasModelException('FIAS model can not load bulk data: ' . $e->getMessage());
         }
+    }
+
+    /**
+     * 1. Mapping should be move to model mapper abstract level
+     * and using from Repository... probably.
+     * 2. Expand map array with variable types to prevent wrong type casting based on variable name.
+     */
+
+    /**
+     * @param array $items
+     * @return array
+     * @throws Exception
+     */
+    protected function mapToModelArray(array $items): array
+    {
+        $models = [];
+
+        foreach ($items as $item) {
+            $models[] = $this->mapToModel($item);
+        }
+
+        return $models;
+    }
+
+    /**
+     * @param array $item
+     * @return $this
+     * @throws Exception
+     */
+    protected function mapToModel(array $item): self
+    {
+        $model = clone($this);
+
+        foreach ($item as $field => $value) {
+            if (array_key_exists($field, $this->mapToModel))
+            {
+                $modelFieldName = $this->mapToModel[$field];
+
+                if (false !== strpos($modelFieldName, "Date")) {
+                    $value = new \DateTimeImmutable($value);
+                }
+
+                if (0 === strpos($modelFieldName, "is")) {
+                    $value = (bool)$value;
+                }
+
+                $model->$modelFieldName = $value;
+            }
+        }
+
+        return $model;
     }
 }
