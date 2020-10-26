108100e9da445f764de2406c1e5fdc52c6f41251
a.kurochkin <inspector@list.ru>
2020-10-26 16:58:46 +0600

    Replace backticked notation

--- loadFIASdata.sh
+++ loadFIASdata.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 TMPDIR="/tmp/fias_gar_xml/"
+
 FIASFILE="/tmp/fias_gar_xml.zip"
 
 mkdir -p $TMPDIR
 
-URL=`php -f ./yii parse-fias/get-download-url`
+URL=$(php -f ./yii parse-fias/get-download-url)
 
 wget "$URL" -O $FIASFILE
 
